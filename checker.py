00d3bed1b62469d794c034a5039fc8beca53f687
Jon Bloom <stash@jon.tw>
2015-01-23 14:29:02 -0500

    notifications in notification center

--- checker.py
+++ checker.py
@@ -1,4 +1,5 @@
 import stashy, pprint
+from pync import Notifier
 from collections import defaultdict
 from time import time, sleep
 from subprocess import call
@@ -7,7 +8,7 @@ from config import *
 d = defaultdict(int)
 last_updated = int(time()) * 1000
 
-stash = stashy.connect("http://www-git.server.gvsu.edu:7990", username, password).projects['WT']
+stash = stashy.connect(url, username, password).projects['WT']
 
 while True:
 	try:
@@ -15,10 +16,11 @@ while True:
 		for repo in repos_to_check:
 			pull_requests = list(stash.repos[repo].pull_requests.all(state='MERGED', order="NEWEST"))
 			if pull_requests:
-				merged_time = pull_requests[0]['updatedDate']
+				pr = pull_requests[0]
+				merged_time = pr['updatedDate']
 				d[repo] = merged_time
 				if merged_time > last_updated:
-					print repo + ': new pull request merged'
+					Notifier.notify('New pull request merged', title=repo, open=url+'projects/WT/repos/'+repo+'/pull-requests/'+pr['id']+'/overview')
 					call('blink1-tool --blue', shell=True)
 				else:
 					print repo + ": no new pull requests merged"
--- config_sample.py
+++ config_sample.py
@@ -1,4 +1,5 @@
 #rename this file to config.py
 username = 'usernamne'
 password = 'password'
+url = 'http://www-git.server.gvsu.edu:7990'
 repos_to_check = ['cms4', 'mathinaction']
